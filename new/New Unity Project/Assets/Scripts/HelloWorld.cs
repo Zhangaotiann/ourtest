8618766899064e71392a71eb2139a0722f724097
Zhangaotiann <8474238@qq.com>
2021-04-27 09:19:22 +0800

    unity

---
+++ new/New Unity Project/Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour 
+{
+    // Start is called before the first frame update //显示第一帧之前 初始化内容
+    void Start()
+    {
+        Debug.Log("Hello Unity");
+    }
+
+    // Update is called once per frame //每一帧都会调用 
+    void Update()
+    {
+        Debug.Log("Hello Unity");
+    }
+}
---
+++ new/New Unity Project/Assets/Scripts/Playercontrol1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playercontrol1 : MonoBehaviour
+{
+    public float force = 5;
+    private Rigidbody2D heroBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        heroBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        //  if ()
+        heroBody.AddForce(Vector2.right * h * force);
+    }
+}
